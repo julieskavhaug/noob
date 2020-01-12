83b686ef12cd5da8da80018c0a41f458f0729d6d
julieskavhaug <54905792+julieskavhaug@users.noreply.github.com>
2020-01-12 21:12:20 +0100

    Update 2017.js

--- 2017.js
+++ 2017.js
@@ -10,6 +10,7 @@ class Firma {
         let samletLoenn = 0;
         for(let i in this.ansattListe){
             samletLoenn += this.ansattListe[i].beregnLoenn();
+            alert(i)
         }
         console.log("Samlet maanedslønn: " + samletLoenn.toFixed(2));
     }
@@ -51,3 +52,15 @@ class FunktionerAnsatt{
                 return this.maanedsLoenn
             }
 }
+
+const TimeLoennAnsatt1= new TimeLoennAnsatt("100176-2312", 185, 37.5);
+const TimeLoennAnsatt2 = new TimeLoennAnsatt("131282-2312", 200, 32);
+
+const funktionerAnsatt1 = new FunktionerAnsatt("05667-0213", 35000);
+const funktionerAnsatt2 = new FunktionerAnsatt("230865-0213", 42000);
+
+
+const firma = new Firma();
+
+firma.tilfoejansatt(TimeLoennAnsatt1, TimeLoennAnsatt2);
+firma.tilfoejansatt(funktionerAnsatt1, funktionerAnsatt2);
