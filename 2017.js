77daefb9a825662b4d09ae8b0357bc0fb87d765a
julieskavhaug <54905792+julieskavhaug@users.noreply.github.com>
2020-01-13 12:31:45 +0100

    Update 2017.js

--- 2017.js
+++ 2017.js
@@ -10,11 +10,11 @@ class Firma {
         let samletLoenn = 0;
         for(let i in this.ansattListe){
             samletLoenn += this.ansattListe[i].beregnLoenn();
-            alert(i)
         }
-        console.log("Samlet maanedslønn: " + samletLoenn.toFixed(2));
+        console.log("Samlet månedslønn: " + samletLoenn.toFixed(2));
     }
 }
+
 
 
 //ANSATT
@@ -22,36 +22,37 @@ class Ansatt {
     constructor(cprNr){
         this.CPRnr = cprNr;
     }
-    beregnloenn(){
-        return null;
+    beregnLoenn(ansatt){
+        return 0.0;
     }
 }
 
 
 //TIMELØNN ANSATT
-class TimeLoennAnsatt{
+class TimeLoennAnsatt extends Ansatt{
     constructor(cprNr, timeLoenn, timerPrUke){
+        super(cprNr);
         this.TimeLoenn = timeLoenn;
         this.TimerPrUke = timerPrUke;
     }
     beregnLoenn(){
         const ukerPrMaaned = 4;
         return this.TimeLoenn * this.TimerPrUke * ukerPrMaaned;
-
     }
-
-
-
 }
+
+
 //FUNKSJONER ANSATT
-class FunktionerAnsatt{
+class FunktionerAnsatt extends Ansatt{
             constructor(cprNr, maanedsLoenn){
+                super(cprNr);
                 this.maanedsLoenn = maanedsLoenn;
             }
             beregnLoenn(){
                 return this.maanedsLoenn
             }
 }
+
 
 const TimeLoennAnsatt1= new TimeLoennAnsatt("100176-2312", 185, 37.5);
 const TimeLoennAnsatt2 = new TimeLoennAnsatt("131282-2312", 200, 32);
@@ -62,5 +63,9 @@ const funktionerAnsatt2 = new FunktionerAnsatt("230865-0213", 42000);
 
 const firma = new Firma();
 
-firma.tilfoejansatt(TimeLoennAnsatt1, TimeLoennAnsatt2);
-firma.tilfoejansatt(funktionerAnsatt1, funktionerAnsatt2);
+firma.tilfoejansatt(TimeLoennAnsatt1);
+firma.tilfoejansatt(TimeLoennAnsatt2);
+firma.tilfoejansatt(funktionerAnsatt1);
+firma.tilfoejansatt(funktionerAnsatt2);
+
+console.log(firma.loennBeregning());
